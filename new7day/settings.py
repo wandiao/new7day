48e8734eff3e7b58ac16eb7c085ec5f9b528f967
wandiao <851298395@qq.com>
2018-08-26 14:01:02 +0800

    config: modify mysql config

--- new7day/settings.py
+++ new7day/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'common',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST':'localhost',
         'PORT':'3306',
     }
