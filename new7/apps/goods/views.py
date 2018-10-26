77754299a638f33567441cf2885bab2fee174606
wandiao <851298395@qq.com>
2018-10-26 22:11:30 +0800

    add cost api

--- new7/apps/goods/views.py
+++ new7/apps/goods/views.py
@@ -1,11 +1,17 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
 from rest_framework import viewsets
 from django.db.models import Sum
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 from new7 import models
 from new7.common import serializers as common_serializers
 from new7.common.schema import auto_schema, DocParam
+from rest_framework.schemas import AutoSchema
+import coreapi
 from rest_framework.response import Response
 from rest_framework import (
     viewsets,
@@ -13,7 +19,7 @@ from rest_framework import (
     status,
     serializers as rest_serializers
 )
-from rest_framework.decorators import list_route, detail_route
+from rest_framework.decorators import list_route, detail_route, schema
 
 from . import filters
 from new7.common.pagination import Pagination
@@ -41,7 +47,7 @@ class GoodsViewSet(viewsets.ModelViewSet):
   商品库存
 
   cost:
-  商品库存
+  商品成本
 
   delete:
   删除商品
@@ -66,10 +72,33 @@ class GoodsViewSet(viewsets.ModelViewSet):
     serializer = self.get_serializer(goods, many=True)
     return paginator.get_paginated_response(serializer.data)
 
-  @list_route()
+  @swagger_auto_schema(method='get', manual_parameters=[
+    openapi.Parameter('start_time', openapi.IN_QUERY, description="开始时间(xxxx-xx-xx)", type=openapi.TYPE_STRING),
+    openapi.Parameter('end_time', openapi.IN_QUERY, description="结束时间(xxxx-xx-xx)", type=openapi.TYPE_STRING),
+    openapi.Parameter('depot', openapi.IN_QUERY, description="库房", type=openapi.TYPE_STRING),
+    
+  ])
+  @list_route(methods=['get'])
   def cost(self, request, *args, **kwargs):
-    records = models.GoodsRecord.objects.values('goods', 'goods__name').annotate(count = Sum('count'), cost=Sum('amount')).all()
-    print(records)
+    queryset = models.GoodsRecord.objects.all()
+    start_time = self.request.GET.get('start_time', None)
+    end_time = self.request.GET.get('end_time', None)
+    depot = self.request.GET.get('depot', '')
+    end_time = datetime.datetime.strptime(
+                end_time, '%Y-%m-%d') if end_time else datetime.datetime.now()
+    if start_time:
+      start_time = datetime.datetime.strptime(start_time, '%Y-%m-%d')
+      queryset = queryset.filter(
+        record_time__gte=start_time,
+        record_time__lte=end_time,
+      )
+    else:
+      queryset = queryset.filter(
+        record_time__lte=end_time,
+      )
+    if depot:
+      queryset = queryset.filter(record_depot=depot)
+    records = queryset.values('goods', 'goods__name').annotate(count = Sum('count'), cost=Sum('amount'))
     serializer = self.get_serializer(records, many=True)
     return Response(serializer.data)
 
